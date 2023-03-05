7d6e1695507b1b261d48eb1865d7ebbea0a60441
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2023-03-05 23:44:44 +0900

    V1.2 출력 방식 변경으로 링크 버그 해결, 삭제 버튼 추가

--- src/main/java/MH/web/IgdbController.java
+++ src/main/java/MH/web/IgdbController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IgdbController {
 
 	@GetMapping("/")
-	public String story() {
-
+	public String index() {
 		return "index";
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "test";
 	}
 }
